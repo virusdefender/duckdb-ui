644d2d4d9eac213004ccdff3e0368b0386386d92
Yves <yves@motherduck.com>
2025-03-11 12:44:39 +0100

    Simplify code

--- src/http_server.cpp
+++ src/http_server.cpp
@@ -64,11 +64,7 @@ bool HttpServer::IsRunningOnMachine(ClientContext &context) {
   auto local_url = StringUtil::Format("http://localhost:%d", local_port);
 
   httplib::Client client(local_url);
-  auto result = client.Get("/info");
-  if (result) {
-    return true;
-  }
-  return false;
+  return client.Get("/info");
 }
 
 bool HttpServer::Started() {
